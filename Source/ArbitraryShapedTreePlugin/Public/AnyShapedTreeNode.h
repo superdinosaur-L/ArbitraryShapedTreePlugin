3b8db21351d8146e0e575c1e3fb489b5c330e52d
RainButterfly <2771960681@qq.com>
2025-10-31 12:44:33 +0800

    Update AnyShapedTreeNode.h

--- Source/ArbitraryShapedTreePlugin/Public/AnyShapedTreeNode.h
+++ Source/ArbitraryShapedTreePlugin/Public/AnyShapedTreeNode.h
@@ -1,3 +1,4 @@
+// Copyright 2024 RainButterfly. All Rights Reserved.
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
@@ -84,4 +85,5 @@ protected:
 
 
 };
+
 
